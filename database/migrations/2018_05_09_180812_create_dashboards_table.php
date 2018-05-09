eb2fc0ddc796ea6ff41f79603438a096b7cc56ff
westbrown1 <lawsonsdad@gmail.com>
2018-05-09 19:18:08 -0400

    try again

--- database/migrations/2018_05_09_180812_create_dashboards_table.php
+++ database/migrations/2018_05_09_180812_create_dashboards_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddDashboardsTableToSwimdash extends Migration
+class CreateDashboardsTable extends Migration
 {
     /**
      * Run the migrations.
