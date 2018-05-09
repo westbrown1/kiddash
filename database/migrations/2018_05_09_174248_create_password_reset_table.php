e024371333cd8f41904559498ed0085348ee2f2e
westbrown1 <lawsonsdad@gmail.com>
2018-05-09 18:55:10 -0400

    yes

--- database/migrations/2018_05_09_174248_create_password_reset_table.php
+++ database/migrations/2018_05_09_174248_create_password_reset_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddPasswordResetsTable extends Migration
+class CreatePasswordResetTable extends Migration
 {
     /**
      * Run the migrations.
