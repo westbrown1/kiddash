f590d545645e3f284dfc9dc688fdddb3f954d6e8
westbrown1 <lawsonsdad@gmail.com>
2018-05-09 00:28:21 -0400

    migrations added

--- database/migrations/2018_05_08_231937_create_myimages_table.php
+++ database/migrations/2018_05_08_231937_create_myimages_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateNotificationsTable extends Migration
+class CreateMyimagesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,12 @@ class CreateNotificationsTable extends Migration
      */
     public function up()
     {
-        Schema::create('notifications', function (Blueprint $table) {
-            $table->uuid('id')->primary();
-            $table->string('type');
-            $table->morphs('notifiable');
-            $table->text('data');
-            $table->timestamp('read_at')->nullable();
+        Schema::create('myimages', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('name');
+            $table->longText('your');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
             $table->timestamps();
         });
     }
@@ -30,6 +30,6 @@ class CreateNotificationsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('notifications');
+        Schema::dropIfExists('myimages');
     }
 }
