859d7f41eedb4b324a5bbdb1648f6ca2b011a519
westbrown1 <lawsonsdad@gmail.com>
2018-05-09 21:16:54 -0400

    redirectifauthenticated

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/dashboards';
 
     /**
      * Create a new controller instance.
