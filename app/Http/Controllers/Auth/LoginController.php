d26daad7786d8108659dda0053721b8c0927d54e
westbrown1 <lawsonsdad@gmail.com>
2018-05-09 21:10:29 -0400

    login redirect

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/dashboards';
+    protected $redirectTo = '/home';
 
     /**
      * Create a new controller instance.
--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -27,7 +27,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/dashboards';
+    protected $redirectTo = '/home';
 
     /**
      * Create a new controller instance.
