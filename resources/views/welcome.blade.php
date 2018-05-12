3cfb2ba98895bbef5b11c692d19382935f4108f9
westbrown1 <lawsonsdad@gmail.com>
2018-05-12 01:34:51 -0400

    google verification code

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,8 +1,8 @@
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
 <head>
-
-google-site-verification: googlebf4fda6c30c7bd3d.html
+    
+    <meta name="google-site-verification" content="AQxOvldFToqna4lAycyg8HvkP54cv03bC_0WBCskKkg" />
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
