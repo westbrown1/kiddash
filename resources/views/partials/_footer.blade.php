b6101959f1e682f6b7a92ecafff6fa5945d0a42b
westbrown1 <lawsonsdad@gmail.com>
2018-06-28 13:21:13 -0400

    working on mobile friendly

---
+++ resources/views/partials/_footer.blade.php
@@ -0,0 +1,16 @@
+<style>
+	#main-footer {
+		background-color: black;
+		margin-bottom: none;
+		bottom: 0px;
+	}
+</style>
+
+
+<div class=row> 
+	<div class=col-md-12 id=main-footer> 
+		<div class="container">
+			<br>
+		<p>To see an example dashboard page, just click <a href="{{ url('/example') }}">here</a></p> 
+		<p>If you would like to contact swim-dash.com, click <a href="mailto:swimdash18@gmail.com">here.</a></p> <p class="pull-right">Copyright &copy; {{ date('Y') }} - swim-dash.com</p></div></div> 
+</div> 
