8da01a4384b4565ff9b0fc6b2aae0a5dd759ba99
westbrown1 <lawsonsdad@gmail.com>
2018-07-04 18:10:02 -0400

    moved example page link

--- resources/views/partials/_footer.blade.php
+++ resources/views/partials/_footer.blade.php
@@ -11,6 +11,5 @@
 	<div class=col-md-12 id=main-footer> 
 		<div class="container">
 			<br>
-		<p>To see an example dashboard page, just click <a href="{{ url('/example') }}">here</a></p> 
-		<p>If you would like to contact swim-dash.com, click <a href="mailto:swimdash18@gmail.com">here.</a></p> <p class="pull-right">Copyright &copy; {{ date('Y') }} - swim-dash.com</p></div></div> 
+			<p>If you would like to contact swim-dash.com, click <a href="mailto:swimdash18@gmail.com">here.</a></p> <p class="pull-right">Copyright &copy; {{ date('Y') }} - swim-dash.com</p></div></div> 
 </div> 
