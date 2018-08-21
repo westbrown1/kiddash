86a99fd57b6c151696d6d0e96a1fda324662a2d5
westbrown1 <lawsonsdad@gmail.com>
2018-08-21 19:10:41 -0400

    update 8-21-18

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -77,8 +77,7 @@
                                                      document.getElementById('logout-form').submit();">
                                             Logout
                                         </a>
-                                        
-                                        <li><a href="messages">Messages @include('messenger.unread-count')</a></li>
+                                        <li><a href="/messages">Messages @include('messenger.unread-count')</a></li>
                                         {{-- <li><a href="/messages/create">Create New Message</a></li> --}}
                                         <li><a href="{{ route('teams.create') }}">Create New Team</a></li>
                                         <li><a href="{{ route('pictures.create') }}">Add a Dashboard Photo/Image</a></li>
